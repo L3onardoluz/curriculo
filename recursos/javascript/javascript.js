6fea6c67ca74db4ec898e2f13c0c0243bf31d214
L3onardoluz <odranoelmob@gmail.com>
2017-12-08 03:48:17 -0200

    versão com menus de exemplos

---
+++ recursos/javascript/javascript.js
@@ -0,0 +1,47 @@
+/* Quando clica no botão, o menu abre ou fecha */
+function abreMenu(obj) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+    obj.classList.toggle("show");
+}
+
+// Menu fecha se clicar fora
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+/*le texto*/
+
+function readTextFile(file)
+{
+    var fileDisplayArea = document.getElementById('conteudo');
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                fileDisplayArea.innerHTML = allText 
+            }
+        }
+    }
+    rawFile.send(null);
+}
